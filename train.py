3804fbd8f99845b6ae56bb3ffe1a5d95643f2776
Vicondrus <victor.padurean@yahoo.com>
2020-03-12 12:55:10 +0200

    added training monitor

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import matplotlib
-from TrafficSignNet import TrafficSignNet
+
+from trafficSignCnn import TrafficSignNet
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
@@ -7,64 +8,91 @@ from sklearn.metrics import classification_report
 from skimage import transform
 from skimage import exposure
 from skimage import io
+
 import matplotlib.pyplot as plot
+
 import numpy as np
 import argparse
 import random
 import os
 
+from keras.callbacks import ModelCheckpoint
+from trainingMonitor import TrainingMonitor
+
 matplotlib.use("Agg")
 
-def load_split(basePath, csvPath):
-    data=[]
-    labels=[]
 
+def load_data_and_labels(basePath, csvPath):
+    data = []
+    labels = []
+
+    # count the number of images
     rows = open(csvPath).read().strip().split("\n")[1:]
     random.shuffle(rows)
 
+    # for each image
     for (i, row) in enumerate(rows):
-        if i>0 and i % 1000 == 0:
+
+        if i > 0 and i % 1000 == 0:
             print("[INFO] processed {} total images".format(i))
 
+        # find path and id
         (label, imagePath) = row.strip().split(",")[-2:]
 
-        imagePath = os.path.sep.join([basePath,imagePath])
+        imagePath = os.path.sep.join([basePath, imagePath])
         image = io.imread(imagePath)
 
-        image = transform.resize(image, (32,32))
+        # resize and
+        image = transform.resize(image, (32, 32))
+
+        # Contrast Limited Adaptive Histogram Equalization (CLAHE).
+        #
+        # An algorithm for local contrast enhancement, that uses histograms computed over different tile regions of
+        # the image. Local details can therefore be enhanced even in regions that are darker or lighter than most of
+        # the image.
         image = exposure.equalize_adapthist(image, clip_limit=0.1)
 
+        # append the transformed image to the data array
         data.append(image)
+        # and the given label too
         labels.append(int(label))
 
     data = np.array(data)
-    label = np.array(labels)
+    labels = np.array(labels)
 
-    return (data, labels)
+    return data, labels
+
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-d","--dataset",required=True,help="path to input training model")
+ap.add_argument("-d", "--dataset", required=True, help="path to input training model")
 ap.add_argument("-m", "--model", required=True, help="path to output model")
+ap.add_argument("-o", "--output", required=True, help="path to output dictionary")
 ap.add_argument("-p", "--plot", type=str, default="plot.png", help="path to training history plot")
-args=vars(ap.parse_args())
+args = vars(ap.parse_args())
 
-NUM_EPOCHS=30
-INIT_LR=1e-3
-BS=64
+# default parameters, might need adjustment
+NUM_EPOCHS = 30
+INIT_LR = 1e-3
+BS = 64
 
 labelNames = open("signnames.csv").read().strip().split("\n")[1:]
 labelNames = [l.split(",")[1] for l in labelNames]
 
-trainPath = os.path.sep.join([args["dataset"],"Train.csv"])
+trainPath = os.path.sep.join([args["dataset"], "Train.csv"])
 testPath = os.path.sep.join([args["dataset"], "Test.csv"])
 
+# load data to a tuple with the previous function
 print("[INFO] loading training and testing data...")
-(trainX, trainY) =load_split(args["dataset"], trainPath)
-(testX, testY) = load_split(args["dataset"], testPath)
+# train data
+(trainX, trainY) = load_data_and_labels(args["dataset"], trainPath)
+# test data
+(testX, testY) = load_data_and_labels(args["dataset"], testPath)
 
+# scale to [0, 1]
 trainX = trainX.astype("float32") / 255.0
 testX = testX.astype("float32") / 255.0
 
+# convert to one-hot encoding (boolean values from which only one is true at a time)
 numLabels = len(np.unique(trainY))
 trainY = to_categorical(trainY, numLabels)
 testY = to_categorical(testY, numLabels)
@@ -88,13 +116,19 @@ opt = Adam(lr=INIT_LR, decay=INIT_LR / (NUM_EPOCHS * 0.5))
 model = TrafficSignNet.build(width=32, height=32, depth=3, classes=numLabels)
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
+# training monitor
+figPath = os.path.sep.join([args["output"], "{}.png".format(os.getpid())])
+jsonPath = os.path.sep.join([args["output"], "{}.json".format(os.getpid())])
+callbacks = [TrainingMonitor(figPath, jsonPath=jsonPath)]
+
 print("[INFO] training network...")
 H = model.fit_generator(
     aug.flow(trainX, trainY, batch_size=BS),
     validation_data=(testX, testY),
-    steps_per_epoch=trainX.shape[0]//BS,
+    steps_per_epoch=trainX.shape[0] // BS,
     epochs=NUM_EPOCHS,
     class_weight=classWeight,
+    callbacks=callbacks,
     verbose=1
 )
 
---
+++ trainingMonitor.py
@@ -0,0 +1,67 @@
+from functools import singledispatch
+
+from keras.callbacks import BaseLogger
+import matplotlib.pyplot as plot
+import numpy as np
+import json
+import os
+
+
+@singledispatch
+def to_serializable(self):
+    """Used by default."""
+    return str(self)
+
+
+@to_serializable.register(np.float32)
+def ts_float32(self):
+    """Used if *val* is an instance of numpy.float32."""
+    return np.float64(self)
+
+
+class TrainingMonitor(BaseLogger):
+
+    def __init__(self, figPath, jsonPath=None, startAt=0):
+        super(TrainingMonitor, self).__init__()
+        self.figPath = figPath
+        self.jsonPath = jsonPath
+        self.startAt = startAt
+
+    def on_train_begin(self, logs={}):
+        self.H = {}
+
+        if self.jsonPath is not None:
+            if os.path.exists(self.jsonPath):
+                self.H = json.loads(open(self.jsonPath).read())
+
+                if self.startAt > 0:
+                    for k in self.H.keys():
+                        self.H[k] = self.H[k][:self.startAt]
+
+    def on_epoch_end(self, epoch, logs={}):
+        for (k, v) in logs.items():
+            l = self.H.get(k, [])
+            l.append(v)
+            self.H[k] = l
+
+        if self.jsonPath is not None:
+            f = open(self.jsonPath, "w")
+            f.write(json.dumps(self.H, default=to_serializable))
+            f.close()
+
+        if len(self.H["loss"]) > 1:
+            N = np.arange(0, len(self.H["loss"]))
+            plot.style.use("ggplot")
+            plot.figure()
+            plot.plot(N, self.H["loss"], label="train_loss")
+            plot.plot(N, self.H["val_loss"], label="val_loss")
+            plot.plot(N, self.H["accuracy"], label="train_acc")
+            plot.plot(N, self.H["val_accuracy"], label="val_acc")
+            plot.title("Training Loss and Accuracy [Epoch {}]".format(
+                len(self.H["loss"])))
+            plot.xlabel("Epoch #")
+            plot.ylabel("Loss/Accuracy")
+            plot.legend()
+
+            plot.savefig(self.figPath)
+            plot.close()
